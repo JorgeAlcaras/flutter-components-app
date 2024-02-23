08a9beb4d51a088e904e19310cb62c72174f5959
Jorge Alcaras <jorge.alcaras@cceo.com.mx>
2024-02-22 19:52:10 -0600

    Feature(Components): :sparkles: The list view builder screen has been modified.

--- lib/screen/listview_builder_screen.dart
+++ lib/screen/listview_builder_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components_app/theme/app_theme.dart';
 
 class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({Key? key}) : super(key: key);
@@ -7,23 +8,48 @@ class ListViewBuilderScreen extends StatefulWidget {
   State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
 }
 
-class _ListViewBuilderScreenState extends State<ListViewBuilderScreen>{
-
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
   final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     scrollController.addListener(() {
-      print('${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}');
-      if ((scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent) {
-        add10();
-      };
+      //print('${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}');
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        fetchData();
+      }
+      ;
     });
   }
 
-  void add10(){
+  Future fetchData() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 2));
+    add10();
+    isLoading = false;
+    setState(() {});
+    if (scrollController.position.pixels + 100 <=
+        scrollController.position.maxScrollExtent) return;
+    scrollController.animateTo(scrollController.position.pixels + 100,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.fastOutSlowIn);
+  }
+
+  Future<void> onRefresh() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    add10();
+  }
+
+  void add10() {
     final lastId = imagesIds.last;
     imagesIds.addAll([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((e) => lastId + e));
     setState(() {});
@@ -31,25 +57,57 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen>{
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: MediaQuery.removePadding(
         context: context,
         removeBottom: true,
         removeTop: true,
-        child: ListView.builder(
-          controller: scrollController,
-            itemCount: imagesIds.length,
-            itemBuilder: (BuildContext context, int index) {
-              return FadeInImage(
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
-                placeholder: const AssetImage('assets/images/loading.gif'),
-                image: NetworkImage('https://picsum.photos/500/300?grayscale=${imagesIds[index]}'),
-          );
-          }
+        child: Stack(children: [
+          RefreshIndicator(
+            color: AppTheme.global,
+            onRefresh: onRefresh,
+            child: ListView.builder(
+                controller: scrollController,
+                itemCount: imagesIds.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return FadeInImage(
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                    placeholder: const AssetImage('assets/images/loading.gif'),
+                    image: NetworkImage(
+                        'https://picsum.photos/500/300?grayscale=${imagesIds[index]}'),
+                  );
+                }),
+          ),
+          if (isLoading)
+            Positioned(
+              bottom: 40,
+              left: size.width / 2 - 30,
+              child: const _LoadingIcon(),
+            ),
+        ]),
       ),
+    );
+  }
+}
+
+class _LoadingIcon extends StatelessWidget {
+  const _LoadingIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.8),
+        shape: BoxShape.circle,
       ),
+      child: const CircularProgressIndicator(color: AppTheme.global),
     );
   }
 }
